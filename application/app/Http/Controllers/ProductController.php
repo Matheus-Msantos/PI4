62d028f94f32540cefb93137611c6763ac7ad5ee
Matheus-Msantos <matheusmelosantos10@gmail.com>
2021-09-04 17:47:17 -0300

    Api Eventos e relação com a tabela categorias

---
+++ application/app/Http/Controllers/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json($product);
+    }
+
+
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json($product);
+    }
+}
--- application/routes/api.php
+++ application/routes/api.php
@@ -3,15 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/*-- Route Product --*/
+Route::get('/product', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::get('/product/{product}', [ProductController::class, 'show']);
+Route::put('/product/{product}', [ProductController::class, 'update']);
+Route::delete('/product/{product}', [ProductController::class, 'destroy']);
 
+/*-- Route Category --*/
 Route::get('/category', [CategoryController::class, 'index']);
-Route::post('/category', [CategoryController::class, 'store']);
 Route::post('/category', [CategoryController::class, 'store']);
 Route::get('/category/{category}', [CategoryController::class, 'show']);
 Route::put('/category/{category}', [CategoryController::class, 'update']);
