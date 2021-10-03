eec998825e6ddae56bdda7a640d04668bc3faaba
Bruno Pontes <bruno_confessor@hotmail.com>
2021-10-03 00:03:41 -0300

    ajuste em menu, ajustes nas views de category, user e css

--- application/resources/views/category/index.blade.php
+++ application/resources/views/category/index.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.head')
 @section('content')
+{{-- <link rel="stylesheet" href="{{asset('css/base.css')}}"> --}}
 
-<body class="container">
+<body class="c-section-container">
 
     @if(session()->has('success'))
         <div class="alert alert-info" role="alert">
             {{ session()->get('success') }}
         </div>
     @endif
-
-    {{-- campo para menu --}}
 
     <div class="mt-5 input-group rounded justify-content-center">
         <span class="input-group-text border-0 bg-white" id="search-addon">
@@ -21,39 +20,38 @@
     </div>
 
     <div class="d-flex justify-content-center">
-        <h1 class="mt-5 text-center fw-bold" id="titleIndex">Lista de Categorias</h1>
+        <h1 class="c-section-title mt-5 text-center fw-bold" id="titleIndex">Lista de Categorias</h1>
 
         <div>
-            <a href="{{ Route('category.create') }}" class="btn btn-warning text-white" id="btn-plus">
+            <a href="{{ Route('category.create') }}" class="c-section-table--button btn btn-warning text-white" id="btn-plus">
                 <i class="fas fa-plus"></i>
             </a>
         </div>
     </div>
 
-    <table class="table table-borderless">
-        <thead>
-            <tr class="text-center text-primary text-uppercase">
-                <th>ID</th>
-                <th>Nome</th>
+    <table class="c-section-table table table-borderless">
+        <thead class="c-section-table--head">
+            <tr class="c-section-table--row text-center text-primary text-uppercase">
+                <th class="c-section-table--header">ID</th>
+                <th class="c-section-table--header">Nome</th>
             </tr>
         <thead>
 
-        <tbody>
+        <tbody class="c-section-table--body">
             @foreach($categories as $category)
-                <tr class="text-center border-bottom">
-                    <td>{{ $category->id }}</td>
-                    <td>{{ $category->name }}</td>
-                    <td>
-                        <a href="#" class="btn btn-success" style="border-radius: 100%">
+                <tr class="c-section-table--head text-center border-bottom">
+                    <td class="c-section-table--data">{{ $category->id }}</td>
+                    <td class="c-section-table--data">{{ $category->name }}</td>
+                    <td class="c-section-table--data">
+                        <a href="#" class="c-section-table--button btn btn-success" style="border-radius: 100%">
                             <i class="fas fa-eye text-white"></i>
                         </a>
-                        <a href="{{ Route('category.edit', $category->id) }}" class="btn btn-warning" style="border-radius: 100%">
+                        <a href="{{ Route('category.edit', $category->id) }}" class="c-section-table--button btn btn-warning" style="border-radius: 100%">
                             <i class="fas fa-pen text-white"></i>
                         </a>
-                        <a onclick="remove('{{ Route('category.destroy', $category->id) }}');" href="#" class="btn btn-danger" style="border-radius: 100%">
+                        <a onclick="remove('{{ Route('category.destroy', $category->id) }}');" href="#" class="c-section-table--button btn btn-danger" style="border-radius: 100%">
                             <i class="fas fa-trash-alt text-white"></i>
                         </a>
-                        <hr>
                     </td>
                 </tr>
             @endforeach
--- application/resources/views/layouts/head.blade.php
+++ application/resources/views/layouts/head.blade.php
@@ -8,6 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/8455a3d02b.js" crossorigin="anonymous"></script>
     <script src="{{ asset('js/menu.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
 
@@ -21,7 +22,7 @@
         <nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
             <ul class="nav sidebar-nav">
                 <div class="m-4 mb-5">
-                    <img class="img-thumbnail" src="{{ asset('image/gnose.png') }}" alt="Brand" style="border-radius: 100%;">
+                    <img class="img-thumbnail" src="{{ asset('image/gnose.png') }}" alt="Brand">
                 </div>
                 <div class="mt-5 mb-5">
                     <li class="mb-1 p-1">
@@ -31,12 +32,12 @@
                         <a class="button pr-5 pl-5 btn btn-light text-primary font-weight-bold" href="{{ Route('category.index') }}" style="border-radius: 30px">CATEGORIAS</a>
                     </li>
                     <li class="mb-1 p-1">
-                        <a class="button pr-5 pl-5 btn btn-light text-primary font-weight-bold" href="#events" style="border-radius: 30px">USUÁRIOS</a>
+                        <a class="button pr-5 pl-5 btn btn-light text-primary font-weight-bold" href="{{ Route('user.index') }}" style="border-radius: 30px">USUÁRIOS</a>
                     </li>
                 </div>
             </ul>
             <div class="p-4 mt-5">
-                <a class="pr-5 pl-5 btn btn-secundary font-weight-bold" href="#sair" style="border-radius: 30px">SAIR</a>
+                <a class="pr-5 pl-5 btn btn-secundary text-white font-weight-bold" href="#sair" style="border-radius: 30px">SAIR</a>
             </div>
         </nav>
         <!-- /#sidebar-wrapper -->
@@ -48,7 +49,7 @@
                      <span class="hamb-middle"></span>
                      <span class="hamb-bottom"></span>
                  </button>
-            <!-- APAGAR ESSA DIV CONTAINDER, É O CONTEUDO DO BORY -->
+            <!-- APAGAR ESSA DIV CONTAINER, É O CONTEUDO DO BORY -->
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2">
